033c8a48e6f6ac45525657e11c06bdc994792510
KrushnaKale07 <krushnakale0007@gmail.com>
2025-06-10 20:05:21 +0530

    User details added

--- src/main/java/com/dailycodebuffer/security/config/WebSecurityConfig.java
+++ src/main/java/com/dailycodebuffer/security/config/WebSecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +22,20 @@ public class WebSecurityConfig {
 		
 		
 		return httpSecurity.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails krushna = 
+				User.withUsername("krushna")
+				.password("{noop}password")
+				.roles("USER").build();
+		
+		UserDetails nikhil = User
+				.withUsername("nikhil")
+				.password("{noop}password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(krushna, nikhil);
 	}
 }
